a6adcd59a0d3f36ea8a42d718616bc3a7311d046
MiroslavTretiak <miroslav.tretiak01@gmail.com>
2024-10-01 15:51:55 +0300

    User registration and delete test case

---
+++ cypress/e2e/registerAndDeleteUser.cy.js
@@ -0,0 +1,42 @@
+describe('Automation Exercice - Register and Delete user', () => {
+  const userData = {
+    name: 'Testuser',
+    email: `Testuse${Date.now()}@example.com`,
+    password: 'Qwerty123',
+    firstName: 'Test',
+    lastName: 'User',
+    company: 'TestCompany',
+    address1: 'TestStreet',
+    address2: 'Bird 56',
+    country: 'United States',
+    state: 'California',
+    city: 'Los Angeles',
+    zipcode: '90001',
+    mobileNumber: '1234567890',
+    birthDay: '10',
+    birthMonth: 'May',
+    birthYear: '1990',
+
+  };
+  it('should register a new user successfully', () =>{
+    cy.visit('http://automationexercise.com');
+
+    cy.get('body').should('contain', 'Home');
+
+    cy.registerUser(userData);
+
+    cy.contains(`Logged in as ${userData.name}`).should('be.visible');
+  });
+    it('should delete user successfully', () =>{
+
+      cy.login(userData.email, userData.password);
+
+    cy.contains('Delete Account').click();
+
+    cy.contains('Account Deleted!').should('be.visible');
+    cy.get('[data-qa="continue-button"]').click();
+
+
+
+  });
+});
